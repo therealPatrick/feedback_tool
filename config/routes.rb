9a51927229331ab8ddddb709b41157aa24770c74
therealPatrick <malamashangala13@gmail.com>
2024-06-03 16:43:18 +0200

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pricing', to: 'marketingpage#pricing' 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
